ae7f501ac11c941533b70e3a5829e1775a2b68d6
YelChristensen <yelena.christensen@gmail.com>
2018-10-25 14:44:00 +0100

    added existing and new guest login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,47 @@
 import React from "react";
 import "../styles/Header.scss";
 
-function Header() {
-  return (
-    <header className="header">
-      <h1>
-        <div className="dropdown">
-          <a className="dropdown__btn" href="/">
-            <i className="fas fa-1x fa-bars" />
-          </a>
+class Header extends React.Component {
+  constructor() {
+    super();
 
-          <div className="dropdown__content">
-            <a href="#">Guest Login</a>
-            <a href="#">Host Login</a>
-            <a href="#">FAQ</a>
-            <a href="#">Contact Us</a>
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleClick(e){
+      e.preventDefault();
+      this.props.switchScreen(e.target.name);
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <h1>
+          <div className="dropdown">
+            <a onClick={this.handleSubmit} className="dropdown__btn" href="/">
+              <i className="fas fa-1x fa-bars" />
+            </a>
+
+            <div className="dropdown__content">
+              <a onClick={this.handleClick} name="guestLogin" href="#">Guest Login</a>
+              <a onClick={this.handleClick} name="hostLogin" href="#">Host Login</a>
+              <a onClick={this.handleClick} name="faq" href="#">FAQ</a>
+              <a onClick={this.handleClick} name="contact" href="#">Contact Us</a>
+            </div>
           </div>
-        </div>
 
-        <a href="/" className="margin-left">
-          Ourbnb
-        </a>
-      </h1>
-    </header>
-  );
+          <a href="/" className="margin-left">
+            Ourbnb
+          </a>
+        </h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
