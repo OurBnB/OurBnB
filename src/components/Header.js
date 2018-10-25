a9a41fafe15d3484484a0d9ad302aa53cbfce655
YelChristensen <yelena.christensen@gmail.com>
2018-10-25 11:46:35 +0100

    dropdown menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import '../styles/Header.scss';
+import React from "react";
+import "../styles/Header.scss";
 
-function Header () {
-    return (
-        <header className="header">
-            <h1>
-                <a href="/"><i className="fas fa-1x fa-bars" /></a>
-                <a href="/" className="margin-left">Ourbnb</a>
-            </h1>
-        </header>
-    )
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <div className="dropdown">
+        
+            <a className="dropdown__btn" href="/">
+              <i className="fas fa-1x fa-bars" />
+            </a>
+     
+          <div className="dropdown__content">
+            <a href="#">Guest Login</a>
+            <a href="#">Host Login</a>
+            <a href="#">FAQ</a>
+            <a href="#">Contact Us</a>
+          </div>
+        </div>
+        <a href="/" className="margin-left">
+          Ourbnb
+        </a>
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Property.js
+++ src/components/Property.js
@@ -12,70 +12,32 @@ class Property extends React.Component {
         super();
         this.handleToggleDescription = this.handleToggleDescription.bind(this);
         this.formatDate = this.formatDate.bind(this);
-        this.formatDateDB = this.formatDateDB.bind(this);
         this.getCurrency = this.getCurrency.bind(this);
         this.daysDifference = this.daysDifference.bind(this);
         this.pluralise = this.pluralise.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.setButtonClass = this.setButtonClass.bind(this);
-        this.setDescriptionClass = this.setDescriptionClass.bind(this);
-
         this.state = {
-          descriptionToggle: false,
-          name: null,
-          telephone: null,
-          email: null
+            descriptionToggle: false
         }
     }
 
     handleToggleDescription () {
         this.setState({
             descriptionToggle: !this.state.descriptionToggle
-        }, () => {
-          if (this.state.descriptionToggle) {
-            document.location = "#description";
-          }
         })
-        
     }
-
-    handleSubmit(event){
-      event.preventDefault();
-      if (this.state.name, this.state.telephone, this.state.email) {
-        // const { name, telephone, email} = this.state;
-        const today = new Date();
-        const bookingData = Object.assign({}, this.state, 
-          {property_id: this.props.property.id, 
-            guest_id: 1, 
-            date_start: this.formatDateDB(this.props.startDate._d), 
-            date_end: this.formatDateDB(this.props.endDate._d), 
-            date_booked: this.formatDateDB(today)
-          });
-        delete bookingData.descriptionToggle;
-        this.props.addBooking(bookingData);
-      }
-    }
-
-    setButtonClass () {
-      return this.state.name && this.state.telephone && this.state.email ? "booking__button" : "booking__button-inactive";
-    }  
 
     handleChange (event) {
-      this.setState({
-          [event.target.name]: event.target.value
-      })
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     formatDate (dateObject) {
-      const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-      return `${dateObject.getDate()} of ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        return `${dateObject.getDate()} of ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
     }
 
-    formatDateDB (dateObject) {
-      return `${dateObject.getFullYear()}-${dateObject.getMonth()}-${dateObject.getDate()}`;
-    }
-    
     getCurrency (string) {
         return string.toLocaleString("en-GB", {
           style: "currency", 
@@ -93,21 +55,18 @@ class Property extends React.Component {
         return n > 1 ? `s` : ``;
     }
 
-    setDescriptionClass () {
-      const selectedClass = this.state.descriptionToggle ? "property__description-text" : "property__description-hidden"
-      return selectedClass;
-    }
-
     render () {
 
     const lengthOfStay = this.daysDifference(this.props.startDate._d, this.props.endDate._d)
 
     const descriptionText = this.props.property.description;
-
-    let images = [], i = 0, max = 5;
-    while (++i <= max) {
-      images.push(`../static/images/${this.props.property[`image_${i}`]}`)
-    }
+    let images = [
+      `../static/images/${this.props.property.image_1}`,
+      `../static/images/${this.props.property.image_2}`,
+      `../static/images/${this.props.property.image_3}`,
+      `../static/images/${this.props.property.image_4}`,
+      `../static/images/${this.props.property.image_5}`
+    ];
       
     return (
       <div className="property">
@@ -121,7 +80,7 @@ class Property extends React.Component {
               images={images}
               thumb={true}
               loop={true}
-              autoplay={8000}
+              autoplay={5000}
             />
           </div>
           <h2>
@@ -145,18 +104,18 @@ class Property extends React.Component {
               {this.props.property.price_per_night}
             </li>
           </ul>
-        </section>
-        <section className="property__description">
-          <button id="description"
-            className="property__description-btn"
-            onClick={this.handleToggleDescription}
-          >
-            Description
-          </button>
-          <div className={this.setDescriptionClass()}>
-          {this.state.descriptionToggle &&
-            remark().use(reactRenderer).processSync(descriptionText).contents}
-          </div>
+</section>
+<section className="property__description">
+            <button
+                className="property__description__btn"
+                onClick={this.handleToggleDescription}
+                >
+                Description
+            </button>
+            {this.state.descriptionToggle &&
+                remark()
+                .use(reactRenderer)
+                .processSync(descriptionText).contents}
         </section>
           
         <section className="property__map">
@@ -170,12 +129,11 @@ class Property extends React.Component {
                     <li>Check-in date: {this.formatDate(this.props.startDate._d)}</li>
                     <li>Check-out date: {this.formatDate(this.props.endDate._d)}</li>
                     <li>Length of stay: {lengthOfStay} night{this.pluralise(lengthOfStay)}</li>
-                    <li>Price per night: {this.getCurrency(Number(this.props.property.price_per_night))}</li>
-                    <li>Total price: {this.getCurrency(this.props.property.price_per_night * lengthOfStay)}</li>
+                    <li>Price: {this.getCurrency(Number(this.props.property.price_per_night))}</li>
                     <li><input onChange={this.handleChange} className="booking__name" type="text" placeholder="Name" name="name" autoComplete="on" /></li>
                     <li><input onChange={this.handleChange} className="booking__telephone" type="text" placeholder="Telephone" name="telephone" autoComplete="on" /></li>
                     <li><input onChange={this.handleChange} className="booking__email" type="text" placeholder="Email" name="email" autoComplete="on" /></li>
-                    <li><button type="submit" className={this.setButtonClass()}>Book now</button></li>
+                    <li><button type="submit">Book now</button></li>
                 </ul>
             </form>
         </section>
@@ -183,4 +141,5 @@ class Property extends React.Component {
     );
   }
 }
+  
 export default Property;
